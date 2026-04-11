00e35c8d71abaf370374b118e6904e84aa06290b
arjunsinghbhetwal <arjunsinghbhetwaal@gmail.com>
2026-04-11 23:54:59 +0530

    Add files via upload

---
+++ Day_41/Day_41.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 5
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+struct Node* front = NULL;
+struct Node* rear = NULL;
+
+void enqueue(int value){
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if(rear == NULL){
+        front = rear = newNode;
+    }else{
+        rear->next = newNode;
+        rear = newNode;
+    }
+}
+
+void dequeue(){
+    if(front == NULL){
+        printf("-1\n"); //print if the front is empty.
+        return;
+    }
+
+    struct Node* temp = front;
+    printf("%d\n",front->data); //prints the ignored node.
+
+    front = front->next; //jumps to the next node.
+
+    if(front == NULL){
+        rear = NULL;
+    }
+    free(temp); //frees the node from memory.
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    while(n--){
+        int choice;
+        scanf("%d",&choice);
+
+        if(choice == 1){
+            int x;
+            scanf("%d",&x);
+            enqueue(x);
+        }else if(choice == 2){
+            dequeue();
+        }
+    }
+
+    return 0;
+}
